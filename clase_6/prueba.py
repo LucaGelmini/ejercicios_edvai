382aaa0e6957cec141cc9feb5b0fc78e2aa4452f
Luca <lucagelmini@hotmail.com>
2023-08-26 17:40:09 -0300

    run_pyspark_script.sh

--- clase_6/prueba.py
+++ clase_6/prueba.py
@@ -1,8 +1,22 @@
 from pyspark import SparkContext, SparkConf
 
+def cartelitos(func):
+    def wrapper(*args, **kwargs):
+        print("\n=====INICIO=======\n")
+        func(*args, **kwargs)
+        print("\n=====FIN=======\n")
+    return wrapper
+
+@cartelitos  
+def main():
+    print("hola from pyspark")
+        
+
+
+
 if __name__ == "__main__":
     conf = SparkConf().setAppName("MyApp")
     sc = SparkContext(conf=conf)
 
-    # rest of your code goes here
-    print("hola from pyspark")
+    
+    
