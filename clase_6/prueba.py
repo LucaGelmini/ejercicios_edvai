1803556c6b4e7524f0513020a064c9858fbd447c
Luca <lucagelmini@hotmail.com>
2023-08-26 17:46:09 -0300

    run_pyspark_script.sh

--- clase_6/prueba.py
+++ clase_6/prueba.py
@@ -1,15 +1,19 @@
 from pyspark import SparkContext, SparkConf
 
 def cartelitos(func):
+    conf = SparkConf().setAppName("MyApp")
+    sc = SparkContext(conf=conf)
     def wrapper(*args, **kwargs):
         print("\n=====INICIO=======\n")
         func(*args, **kwargs)
         print("\n=====FIN=======\n")
-    return wrapper
+    return wrapper(sc)
 
 @cartelitos  
-def main():
+def main(SparkContext):
     print("hola from pyspark")
+    print(SparkContext.version)
+    
         
 
 
@@ -17,6 +21,7 @@ def main():
 if __name__ == "__main__":
     conf = SparkConf().setAppName("MyApp")
     sc = SparkContext(conf=conf)
+    main()
 
     
     
