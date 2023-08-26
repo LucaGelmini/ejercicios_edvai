654fa899cbe0773418aaa26debe983c102d3e485
Luca <lucagelmini@hotmail.com>
2023-08-26 17:02:05 -0300

    hola from pyspark

--- clase_6/prueba.py
+++ clase_6/prueba.py
@@ -1,1 +1,8 @@
-print("hola")
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("MyApp")
+    sc = SparkContext(conf=conf)
+
+    # rest of your code goes here
+    print("hola from pyspark")
