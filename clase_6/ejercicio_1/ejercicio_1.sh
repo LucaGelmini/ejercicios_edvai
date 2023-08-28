713077cb7988f70f39f919a2f73a6fe3aeeea6b2
Luca <lucagelmini@hotmail.com>
2023-08-28 19:01:07 -0300

    hive

--- clase_6/ejercicio_1/ejercicio_1.sh
+++ clase_6/ejercicio_1/ejercicio_1.sh
@@ -1,7 +1,8 @@
+rm /home/hadoop/landing/yellow_tripdata_2021-01.parquet
 wget -P /home/hadoop/landing https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet
 
 
 hdfs dfs -rm /ingest/yellow_tripdata_2021-01.parquet
-hdfs dfs -put ./yellow_tripdata_2021-01.parquet /ingest
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest
 
 hive -f ./crea_tablas.hql
