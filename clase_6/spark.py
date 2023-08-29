2833145de700d02bdc6c51508519908accda6a29
Luca <lucagelmini@hotmail.com>
2023-08-28 23:29:09 -0300

    pyspark_6

--- clase_6/spark.py
+++ clase_6/spark.py
@@ -1,9 +1,12 @@
 from init_pyspark import init_pyspark
 
+from pyspark import SparkContext
+
 @init_pyspark  
-def main(SparkContext):
+def main(SparkContext: SparkContext):
     print("hola from pyspark")
     print(SparkContext.version)
+    
     
 
 if __name__ == "__main__":
