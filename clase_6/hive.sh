277d5b3a0757e407956ea5dcd4aa4741a687e853
Luca <lucagelmini@hotmail.com>
2023-09-03 20:57:54 -0300

    clase_6

--- clase_6/hive.sh
+++ clase_6/hive.sh
@@ -2,11 +2,11 @@ rm /home/hadoop/landing/yellow_tripdata_2021-01.parquet
 hdfs dfs -rm /ingest/yellow_tripdata_2021-01.parquet
 wget -P /home/hadoop/landing https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet
 
-hdfs dfs -cp /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/congestion/yellow_tripdata_2021-01.parquet
-hdfs dfs -cp /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/distance/yellow_tripdata_2021-01.parquet
-hdfs dfs -cp /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/passengers/yellow_tripdata_2021-01.parquet
-hdfs dfs -cp /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/payments/yellow_tripdata_2021-01.parquet
-hdfs dfs -cp /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/tolls/yellow_tripdata_2021-01.parquet
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/congestion/yellow_tripdata_2021-01.parquet
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/distance/yellow_tripdata_2021-01.parquet
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/passengers/yellow_tripdata_2021-01.parquet
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/payments/yellow_tripdata_2021-01.parquet
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/tolls/yellow_tripdata_2021-01.parquet
 
 
 hive -f ./crea_tablas.hql
