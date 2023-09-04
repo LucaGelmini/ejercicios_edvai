717fb7913fd2bed521e67386fa49d4649a47681a
Luca <lucagelmini@hotmail.com>
2023-09-04 01:03:49 -0300

    clase_6

--- clase_6/hive.sh
+++ clase_6/hive.sh
@@ -1,6 +1,6 @@
 rm /home/hadoop/landing/yellow_tripdata_2021-01.parquet
 hdfs dfs -rm /ingest/yellow_tripdata_2021-01.parquet
-wget -P /home/hadoop/landing https://github.com/LucaGelmini/datasets/blob/main/yellow_tripdata_2021-01.csv
+wget -P /home/hadoop/landing https://media.githubusercontent.com/media/LucaGelmini/datasets/main/yellow_tripdata_2021-01.csv
 
 hdfs dfs -mkdir /ingest/ejercicio_6
 hdfs dfs -mkdir /ingest/ejercicio_6/congestion
