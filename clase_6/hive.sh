391651595b770083dc179eb436f12557556b9fe5
Luca <lucagelmini@hotmail.com>
2023-09-04 00:48:57 -0300

    clase_6

--- clase_6/hive.sh
+++ clase_6/hive.sh
@@ -1,6 +1,6 @@
 rm /home/hadoop/landing/yellow_tripdata_2021-01.parquet
 hdfs dfs -rm /ingest/yellow_tripdata_2021-01.parquet
-wget -P /home/hadoop/landing https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet
+wget -P /home/hadoop/landing https://github.com/LucaGelmini/datasets/blob/main/yellow_tripdata_2021-01.csv
 
 hdfs dfs -mkdir /ingest/ejercicio_6
 hdfs dfs -mkdir /ingest/ejercicio_6/congestion
@@ -9,12 +9,12 @@ hdfs dfs -mkdir /ingest/ejercicio_6/passengers
 hdfs dfs -mkdir /ingest/ejercicio_6/payments
 hdfs dfs -mkdir /ingest/ejercicio_6/tolls
 
-hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/congestion/yellow_tripdata_2021-01.parquet
-hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/distance/yellow_tripdata_2021-01.parquet
-hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/passengers/yellow_tripdata_2021-01.parquet
-hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/payments/yellow_tripdata_2021-01.parquet
-hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.parquet /ingest/ejercicio_6/tolls/yellow_tripdata_2021-01.parquet
-rm /home/hadoop/landing/yellow_tripdata_2021-01.parquet
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.csv /ingest/ejercicio_6/congestion/yellow_tripdata_2021-01.csv
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.csv /ingest/ejercicio_6/distance/yellow_tripdata_2021-01.csv
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.csv /ingest/ejercicio_6/passengers/yellow_tripdata_2021-01.csv
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.csv /ingest/ejercicio_6/payments/yellow_tripdata_2021-01.csv
+hdfs dfs -put /home/hadoop/landing/yellow_tripdata_2021-01.csv /ingest/ejercicio_6/tolls/yellow_tripdata_2021-01.csv
+rm /home/hadoop/landing/yellow_tripdata_2021-01.csv
 
 
 hive -f ./crea_tablas.hql
